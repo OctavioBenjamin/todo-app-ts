e5bf1618468821274f9515f272bcb6636a4b924c
Octavio Benjamin Mendez <97177@sistemas.frc.utn.edu.ar>
2024-07-10 11:28:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Todos } from "./components/Todos";
+import { type Todo as TodoType } from "./types";
+import { TodoId, TodoCompleted } from "./types";
+
+const mockTodos = [
+  {
+    id: "1",
+    title: "Preparar comida",
+    completed: true
+  },
+  {
+    id: "2",
+    title: "Estudiar para la universidad",
+    completed: false
+  },
+  {
+    id: "3",
+    title: "Pasear al perro",
+    completed: false
+  }
+]
+
+const App = () => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ({ id }: TodoId) => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({ id, completed }: Pick<TodoType, "id" | "completed">): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: completed
+        };
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  return ( 
+    <div className="todoapp">
+      <Todos 
+        onToggleCompletedTodo={handleCompleted}
+        onRemoveTodo={handleRemove} 
+        todos={todos}/>
+    </div>
+   )
+}
+
+export default App;
